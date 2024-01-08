bd3c11e3d833fcc3a0c906841d906e9d9a5d97e2
Yahiamostafa11 <yahiawork@gmail.com>
2024-01-08 14:39:30 +0200

    'stamont'

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_strng):
+    new_str = ""
+    for i in my_string:
+        if is not 'c' and i is 'C':
+            new_str += i #new_str = new_str + i
+        return (new_str)
